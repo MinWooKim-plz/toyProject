45d1f6c0d596cd450e3e361cbb616e55d3f66c91
RED <rlaalsdn110@gmail.com>
2024-08-11 08:48:24 +0900

    수정

--- src/main/java/com/example/networker_test/repository/order/ShippingInfoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.networker_test.repository.order;
-
-import com.example.networker_test.domain.order.ShippingInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ShippingInfoRepository extends JpaRepository<ShippingInfo, Long> {
-}
